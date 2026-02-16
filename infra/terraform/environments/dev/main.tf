126e5ca2aaea1b7afd75d5fedd8fcff2ce6783ea
Kenzo Sato <kenzohideaky@gmail.com>
2026-02-15 21:51:01 -0300

    style(tf): fmt

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 module "vpc" {
   source = "../../modules/vpc"
 
-  name_prefix         = var.cluster_name
-  vpc_cidr            = var.vpc_cidr
-  availability_zones  = var.availability_zones
+  name_prefix        = var.cluster_name
+  vpc_cidr           = var.vpc_cidr
+  availability_zones = var.availability_zones
   tags = {
     Environment = var.environment
   }
@@ -16,13 +16,13 @@ module "vpc" {
 module "eks" {
   source = "../../modules/eks"
 
-  cluster_name         = var.cluster_name
-  cluster_version      = var.cluster_version
-  vpc_id               = module.vpc.vpc_id
-  private_subnet_ids   = module.vpc.private_subnet_ids
+  cluster_name        = var.cluster_name
+  cluster_version     = var.cluster_version
+  vpc_id              = module.vpc.vpc_id
+  private_subnet_ids  = module.vpc.private_subnet_ids
   node_instance_types = ["t3.small"]
-  node_desired_size    = 2
-  node_min_size        = 1
-  node_max_size        = 3
-  environment          = var.environment
+  node_desired_size   = 2
+  node_min_size       = 1
+  node_max_size       = 3
+  environment         = var.environment
 }
--- infra/terraform/environments/prod/main.tf
+++ infra/terraform/environments/prod/main.tf
@@ -12,9 +12,9 @@ module "ecr" {
 module "vpc" {
   source = "../../modules/vpc"
 
-  name_prefix         = var.cluster_name
-  vpc_cidr            = var.vpc_cidr
-  availability_zones  = var.availability_zones
+  name_prefix        = var.cluster_name
+  vpc_cidr           = var.vpc_cidr
+  availability_zones = var.availability_zones
   tags = {
     Environment = var.environment
   }
@@ -23,13 +23,13 @@ module "vpc" {
 module "eks" {
   source = "../../modules/eks"
 
-  cluster_name         = var.cluster_name
-  cluster_version      = var.cluster_version
-  vpc_id               = module.vpc.vpc_id
-  private_subnet_ids   = module.vpc.private_subnet_ids
+  cluster_name        = var.cluster_name
+  cluster_version     = var.cluster_version
+  vpc_id              = module.vpc.vpc_id
+  private_subnet_ids  = module.vpc.private_subnet_ids
   node_instance_types = ["t3.small"]
-  node_desired_size    = 2
-  node_min_size        = 1
-  node_max_size        = 3
-  environment          = var.environment
+  node_desired_size   = 2
+  node_min_size       = 1
+  node_max_size       = 3
+  environment         = var.environment
 }
