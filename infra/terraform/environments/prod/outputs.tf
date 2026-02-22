ab9a5c086ab5b7de75837e0286d38ff237c6ee8b
Kenzo Sato <kenzohideaky@gmail.com>
2026-02-22 19:12:10 -0300

    fix: uncomment tf code

--- infra/terraform/environments/prod/outputs.tf
+++ infra/terraform/environments/prod/outputs.tf
@@ -13,22 +13,22 @@ output "lb_controller_role_arn" {
   value       = aws_iam_role.lb_controller.arn
 }
 
-# output "vpc_id" {
-#   description = "ID of the VPC."
-#   value       = module.vpc.vpc_id
-# }
+output "vpc_id" {
+  description = "ID of the VPC."
+  value       = module.vpc.vpc_id
+}
 
-# output "cluster_id" {
-#   description = "EKS cluster ID (name)."
-#   value       = module.eks.cluster_id
-# }
+output "cluster_id" {
+  description = "EKS cluster ID (name)."
+  value       = module.eks.cluster_id
+}
 
-# output "cluster_endpoint" {
-#   description = "Endpoint for the EKS cluster API."
-#   value       = module.eks.cluster_endpoint
-# }
+output "cluster_endpoint" {
+  description = "Endpoint for the EKS cluster API."
+  value       = module.eks.cluster_endpoint
+}
 
-# output "kubeconfig_command" {
-#   description = "Command to update kubeconfig for the EKS cluster."
-#   value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${var.cluster_name}"
-# }
+output "kubeconfig_command" {
+  description = "Command to update kubeconfig for the EKS cluster."
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${var.cluster_name}"
+}
