d727efbaf98bb3f9950ca2e66b75338bdefc5b62
Kenzo Sato <kenzohideaky@gmail.com>
2026-02-23 00:03:26 -0300

    fix: tf fmt

--- infra/terraform/environments/prod/main.tf
+++ infra/terraform/environments/prod/main.tf
@@ -29,7 +29,7 @@ module "eks" {
   vpc_id              = module.vpc.vpc_id
   private_subnet_ids  = module.vpc.private_subnet_ids
   node_instance_types = ["t3.small"]
-  node_desired_size   = 3   # 2 nós + kube-prometheus-stack = "Too many pods"; 3 nós evita
+  node_desired_size   = 3 # 2 nós + kube-prometheus-stack = "Too many pods"; 3 nós evita
   node_min_size       = 1
   node_max_size       = 5
   environment         = var.environment
